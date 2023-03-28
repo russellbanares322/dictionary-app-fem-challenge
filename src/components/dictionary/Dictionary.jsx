1769d1a61c1d9203abce9ce045e033cda9c2c08b
russellbanares322 <banaresrussell11@gmail.com>
2023-03-28 11:51:19 +0800

    feat: add word audio

--- src/components/dictionary/Dictionary.jsx
+++ src/components/dictionary/Dictionary.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoPlaySharp, IoStopSharp } from "react-icons/io5";
 import { TbExternalLink } from "react-icons/tb";
 import useFetchWord from "../../hooks/useFetchWord";
@@ -7,9 +7,24 @@ const Dictionary = () => {
   const [isAudioPlayed, setIsAudioPlayed] = useState(false);
   const { data, error } = useFetchWord();
 
-  const handlePlayAudio = () => {
-    setIsAudioPlayed((prev) => !prev);
+  const handlePlayAudio = (wordAudio) => {
+    const audio = new Audio(wordAudio);
+    audio.play();
+    setIsAudioPlayed(true);
   };
+
+  const handleStopAudio = (wordAudio) => {
+    const audio = new Audio(wordAudio);
+    audio.pause();
+    setIsAudioPlayed(false);
+  };
+
+  useEffect(() => {
+    const modifyAudioIcon = setTimeout(() => {
+      setIsAudioPlayed(false);
+      clearTimeout(modifyAudioIcon);
+    }, 3000);
+  }, [isAudioPlayed]);
   return (
     <div>
       {error && <h1 className="text-center mt-20 text-2xl">{error}</h1>}
@@ -20,17 +35,33 @@ const Dictionary = () => {
               <p className=" text-black font-bold text-4xl">{val?.word}</p>
               <p className="text-purple mt-1 text-lg">{val?.phonetic}</p>
             </div>
-            <div
-              onClick={handlePlayAudio}
-              className="bg-light-purple p-3 rounded-full cursor-pointer"
-            >
-              {!isAudioPlayed && (
-                <IoPlaySharp className="text-purple  pl-1" size={25} />
-              )}
-              {!!isAudioPlayed && (
-                <IoStopSharp className="text-purple pl-1" size={25} />
-              )}
-            </div>
+
+            {val?.phonetics
+              .filter(({ audio }) => audio !== null)
+              .reverse()
+              .slice(0, 1)
+              .map(({ audio }, index) => (
+                <div
+                  onClick={() => handlePlayAudio(audio)}
+                  key={index}
+                  className="bg-light-purple p-3 rounded-full cursor-pointer"
+                >
+                  {!isAudioPlayed && (
+                    <IoPlaySharp
+                      onClick={() => handlePlayAudio(audio)}
+                      className="text-purple  pl-1"
+                      size={25}
+                    />
+                  )}
+                  {!!isAudioPlayed && (
+                    <IoStopSharp
+                      onClick={() => handleStopAudio(audio)}
+                      className="text-purple pl-1"
+                      size={25}
+                    />
+                  )}
+                </div>
+              ))}
           </div>
           <div className="mt-7">
             <p className="font-bold relative after:absolute after:right-0 after:top-[50%] after:bg-light-purple after:w-[90%] after:h-[0.9px] after:block">
@@ -65,7 +96,7 @@ const Dictionary = () => {
                   .filter(({ partOfSpeech }) => partOfSpeech === "noun")
                   .map(({ synonyms }, index) => (
                     <p key={index} className="text-purple font-semibold">
-                      {synonyms?.length === 0 ? "N/A" : synonyms?.join(", ")}
+                      {!synonyms ? "N/A" : synonyms?.join(", ")}
                     </p>
                   ))}
               </div>
@@ -84,16 +115,17 @@ const Dictionary = () => {
                 .map(({ definitions }) =>
                   definitions?.map(({ definition, example }, index) => (
                     <div key={index}>
-                      {!definition && !example ? (
-                        <p className="text-purple">N/A</p>
-                      ) : (
-                        <>
-                          <li className="list-disc text-purple font-light mt-3">
-                            <span className="text-black">{definition}</span>
-                          </li>
-                          <p className="text-gray mt-2">{example}</p>
-                        </>
-                      )}
+                      <>
+                        <li className="list-disc text-purple font-light mt-3">
+                          <span className="text-black">{definition}</span>
+                        </li>
+                        {example?.length > 1 && (
+                          <p className="text-gray mt-2">"{example}"</p>
+                        )}
+                        {example?.length === 0 && (
+                          <p className="text-purple">N/A</p>
+                        )}
+                      </>
                     </div>
                   ))
                 )}
