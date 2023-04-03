cfdf9ecef60d6a7d46a686b3e1f966710f4df073
russellbanares322 <banaresrussell11@gmail.com>
2023-04-03 21:20:29 +0800

    chore: add dictionary content width for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,11 @@ function App() {
 
   return (
     <div
-      className={`flex min-h-[400px]   flex-col justify-center items-center py-10 px-10 md:px-1 transition duration-500 ease-in-out ${fontStyle} ${
+      className={`flex min-h-[400px] flex-col justify-center items-center py-10 px-10 md:px-1 transition duration-500 ease-in-out ${fontStyle} ${
         isDarkTheme ? "bg-dark" : "bg-white"
       }`}
     >
-      <div className="w-[35rem] mx-3 md:mx-2 flex-grow-1">
+      <div className="w-full md:w-[35rem] mx-3 md:mx-2 flex-grow-1 h-full">
         {!!isLoading && (
           <div className="flex items-center justify-center h-screen">
             <BounceLoader color="#A544ED" size={80} />
